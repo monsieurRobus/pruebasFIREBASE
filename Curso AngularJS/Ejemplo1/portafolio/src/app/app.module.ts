21525ab725cdea9f57f8b46227bd157cbf3927b3
Unknown <cdperez.85@gmail.com>
2017-08-18 01:11:42 +0200

    carlosPC: montaje proyecto en angularJS

--- Curso AngularJS/Ejemplo1/portafolio/src/app/app.module.ts
+++ Curso AngularJS/Ejemplo1/portafolio/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
